cebe2e8701a1ac66b585937a5edb8a7e4aafed5d
ootabitbucket <oota.sayaka12@gmail.com>
2018-12-06 13:36:10 +0900

    update

---
+++ src/ot/test/BasicTest.java
@@ -0,0 +1,12 @@
+package ot.test;
+
+import ot.utils.Basic;
+
+public class BasicTest extends Basic{
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//		out("start");
+	}
+
+}
